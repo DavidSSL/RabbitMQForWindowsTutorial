a2ac1e6e21bcd1bd94b7382fa7ea6a4d66992c3c
David Siew <davidsiew@outlook.com>
2013-08-07 17:51:10 +0100

    - Add the RabbitMQ client - Create Producer

---
+++ HelloWorldExample/Producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare("hello-world-queue", false, false, false, null);
+
+            byte[] message = Encoding.UTF8.GetBytes("Hello, World!");
+            channel.BasicPublish(string.Empty, "hello-world-queue",null, message);
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
