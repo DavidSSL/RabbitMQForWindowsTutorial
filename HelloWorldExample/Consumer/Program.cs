ba7489b1a62b579b12ecc32a6e607caf76e7aedc
David Siew <davidsiew@outlook.com>
2013-08-07 18:59:55 +0100

    - Implement Consumer

---
+++ HelloWorldExample/Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+            channel.QueueDeclare("hello-world-queue", false, false, false, null);
+
+            BasicGetResult result = channel.BasicGet("hello-world-queue", true);
+
+            if (result != null)
+            {
+                string message = Encoding.UTF8.GetString(result.Body);
+                Console.WriteLine(message);
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
