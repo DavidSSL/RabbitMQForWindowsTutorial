09fd6d65d2dc39ce9e097de8997175a4d11c8aa5
David Siew <davidsiew@outlook.com>
2013-08-08 10:57:56 +0100

    - Implement logging application

---
+++ LoggingExample/Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string exchange = "direct-exchange-example";
+
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+            channel.QueueDeclare("logs", false, false, true, null);
+            channel.QueueBind("logs", exchange, string.Empty);
+
+            var consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume("logs", true, consumer);
+
+            var eventArgs = (BasicDeliverEventArgs) consumer.Queue.Dequeue();
+
+            string message = Encoding.UTF8.GetString(eventArgs.Body);
+            Console.WriteLine(message);
+
+            channel.Close();
+            connection.Close();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LoggingExample/Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string exchange = "direct-exchange-example";
+
+            Thread.Sleep(1000);
+            var connectionFactory = new ConnectionFactory();
+            IConnection connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange, ExchangeType.Direct);
+            var value = DoSomethingInteresting();
+            var logMessage = string.Format("{0}: {1}", TraceEventType.Information, value);
+
+            var message = Encoding.UTF8.GetBytes(logMessage);
+            
+            channel.BasicPublish(exchange, string.Empty, null, message);
+            
+            channel.Close();
+            connection.Close();
+        }
+
+        private static string DoSomethingInteresting()
+        {
+            return Guid.NewGuid().ToString();
+        }
+    }
+}
