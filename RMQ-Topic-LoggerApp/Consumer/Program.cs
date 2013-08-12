f61f40d6d308634ae4b27258cac03a18e97fb2e9
David Siew <davidsiew@outlook.com>
2013-08-12 17:09:11 +0100

    - Add Consumer to retrieve only informational messages.

---
+++ RMQ-Topic-LoggerApp/Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            const string exchange = "topic-exchange-example";
+            const string queue = "log";
+
+            var connectionFactory = new ConnectionFactory();
+            var connection = connectionFactory.CreateConnection();
+            IModel channel = connection.CreateModel();
+
+            channel.ExchangeDeclare(exchange, ExchangeType.Topic, false, true, null);
+            channel.QueueDeclare("log", false, false, true, null);
+            channel.QueueBind(queue, exchange, "*.Personal.*");
+
+            var consumer = new QueueingBasicConsumer(channel);
+            channel.BasicConsume(queue, true, consumer);
+
+            while (true)
+            {
+                try
+                {
+                    var eventArgs = (BasicDeliverEventArgs) consumer.Queue.Dequeue();
+                    var message = Encoding.UTF8.GetString(eventArgs.Body);
+                    Console.WriteLine(string.Format("{0} - {1}", eventArgs.RoutingKey, message));
+                }
+                catch (EndOfStreamException)
+                {
+                    // The consumer was cancelled, the model closed, or the connection went away.
+                    break;
+                }
+            }
+            channel.Close();
+            connection.Close();
+        }
+    }
+}
